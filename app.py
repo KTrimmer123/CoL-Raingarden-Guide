3857d24e8307d81bac3f803a426f0eefff36ac4e
Enginuity Design <keridarrentrimmer@gmail.com>
2025-05-17 21:02:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,12 @@ st.markdown(
         margin-bottom: 1.25rem;
     }
 
+    .enginuity-logo {
+        display: flex;
+        justify-content: center;
+        margin-top: 2rem;
+    }
+
     .login-heading {
         text-align: center;
         font-family: 'Poppins', sans-serif;
@@ -88,7 +94,7 @@ if not st.session_state.logged_in:
     st.markdown(
         """
         <div class='centered-logo'>
-            <img src='https://raw.githubusercontent.com/KTrimmer123/CoL-Raingarden-Guide/main/assets/City_of_London_logo.svg.png' width='375'/>
+            <img src='https://raw.githubusercontent.com/KTrimmer123/CoL-Raingarden-Guide/main/assets/City_of_London_logo.svg.png' width='300'/>
         </div>
         """,
         unsafe_allow_html=True
@@ -119,7 +125,7 @@ if not st.session_state.logged_in:
     # --- Enginuity Logo (JPG) ---
     st.markdown(
         """
-        <div class='centered-logo'>
+        <div class='enginuity-logo'>
             <img src='https://raw.githubusercontent.com/KTrimmer123/CoL-Raingarden-Guide/main/assets/Enginuity_logo.jpg' width='160'/>
         </div>
         """,

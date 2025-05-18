797c7a64ee82498174a0f6fafbe043f90d2ef7de
Enginuity Design <keridarrentrimmer@gmail.com>
2025-05-18 13:19:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -103,9 +103,7 @@ if not st.session_state.logged_in:
     st.stop()
 
 # --- CALCULATOR PAGE ---
-st.markdown("""
-<div class='login-heading'>City of London<br>Raingarden Guide</div>
-""", unsafe_allow_html=True)
+st.title("City of London Raingarden Guide")
 
 with st.expander("Input Parameters", expanded=False):
     area = st.number_input("Raingarden Area (m²)", min_value=1, value=10, step=1, format="%d")
